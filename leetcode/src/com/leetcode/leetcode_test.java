d0f80dfa104d0d075dfedaae0b02ecd62494a9f4
Lakers-Zsy <1946955309@qq.com>
2019-10-09 15:51:21 +0800

    first project about string find

---
+++ leetcode/src/com/leetcode/leetcode_test.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+public class leetcode_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				int length=lengthOfLongestSubstring.lengthOfLongestSubstring("abba");
+				System.out.println(length);
+		
+	}
+
+}
---
+++ leetcode/src/com/leetcode/lengthOfLongestSubstring.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+public class lengthOfLongestSubstring {
+	
+	 public static int lengthOfLongestSubstring(String s) {
+	        int left=0,max=0;
+	        if(s.length()==0)return 0;
+	         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+	        for(int i=0;i<s.length();i++)
+	        {
+	            if(map.containsKey(s.charAt(i))) 
+	            {
+	            	//实质get函数不会因为left指针偏移而消失，所以left指针以最右结果为主，防止abba情况
+	            	left=Math.max(left,map.get(s.charAt(i)) + 1);
+	            }
+	            
+	            map.put(s.charAt(i),i);
+	            max = Math.max(max,i-left+1);//这部就可以解决123456bb12的情况长度取最长那段 然后后面重复的就从长的那边讨债
+	            
+	        }
+	        
+	        return max;
+	    }
+
+
+
+}
